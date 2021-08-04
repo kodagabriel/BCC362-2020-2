8a6ed4e59a0db3dd2ba76b06c28f4a64105b8250
DESKTOP-17TVKQE\kodag <kodagabriel@outlook.com>
2021-08-03 23:22:10 -0300

    no message

--- pubsub/src/appl/OneAppl.java
+++ pubsub/src/appl/OneAppl.java
@@ -1,13 +1,10 @@
 package appl;
 
-import java.util.ArrayList;
+import core.Message;
+
 import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
-
-import core.Message;
-
-import javax.swing.plaf.synth.SynthOptionPaneUI;
 
 public class OneAppl {
 
@@ -18,7 +15,7 @@ public class OneAppl {
 
     public OneAppl(boolean flag) {
         //Lendo a porta e endereço do client e broker
-        /*
+
         Scanner reader = new Scanner(System.in);  // Reading from System.in
         System.out.print("Enter the client address: ");
         String clientAddress = reader.next();
@@ -28,56 +25,58 @@ public class OneAppl {
         String brokerAddress = reader.next();
         System.out.print("Enter the broker port number: ");
         int brokerPort = reader.nextInt(); // Scans the next token of the input as an int.
-        */
-
-
+        System.out.print("Enter the number of times client should access");
+        int numberOfTimes = reader.nextInt();
+/*
         String brokerAddress = "localhost";
         int brokerPort = 8090;
         String clientAddress = "localhost";
         int clientPort = 8092;
 
-        //Criando client
-        //O client é composto de um name e internamente possui um contador de operações.
-        //A primeira interação com o broker é a operação 1, a segunda, operação 2...
-        //Isso é usado para identificar conjuntos de lock, publish e unlock pertencentes a mesma operação
-        PubSubClient client1 = new PubSubClient("client1", clientAddress, 8092);
-        //PubSubClient client2 = new PubSubClient("client2", clientAddress, 8093);
-        //PubSubClient client3 = new PubSubClient("client3", clientAddress, 8094);
+ */
 
-        //Subscrevendo client
-        client1.subscribe(brokerAddress, brokerPort);
-        //client2.subscribe(brokerAddress, brokerPort);
-        //client3.subscribe(brokerAddress, brokerPort);
+        for (int i = 0; i < numberOfTimes; i++) {
+            //Criando client
+            //O client é composto de um name e internamente possui um contador de operações.
+            //A primeira interação com o broker é a operação 1, a segunda, operação 2...
+            //Isso é usado para identificar conjuntos de lock, publish e unlock pertencentes a mesma operação
+            PubSubClient client1 = new PubSubClient("client1", clientAddress, clientPort);
+            PubSubClient client2 = new PubSubClient("client2", clientAddress, clientPort++);
+            PubSubClient client3 = new PubSubClient("client3", clientAddress, clientPort++);
 
-        //Criando a thread que vai tentar dar lock no topico, publicar e então dar unlock
-        Thread accessOne = new ThreadLockPubUnlock(client1, "varX", brokerAddress, brokerPort);
-        //Thread accessTwo = new ThreadLockPubUnlock(client2, "varX", brokerAddress, brokerPort);
-        //Thread accessThree = new ThreadLockPubUnlock(client3, "varX", brokerAddress, brokerPort);
+            //Subscrevendo client
+            client1.subscribe(brokerAddress, brokerPort);
+            client2.subscribe(brokerAddress, brokerPort);
+            client3.subscribe(brokerAddress, brokerPort);
 
-        //Executando o metodo run da thread
-        accessOne.start();
-        //accessTwo.start();
-        //accessThree.start();
+            //Criando a thread que vai tentar dar lock no topico, publicar e então dar unlock
+            Thread accessOne = new ThreadLockPubUnlock(client1, "varX", brokerAddress, brokerPort);
+            Thread accessTwo = new ThreadLockPubUnlock(client2, "varX", brokerAddress, brokerPort);
+            Thread accessThree = new ThreadLockPubUnlock(client3, "varX", brokerAddress, brokerPort);
 
-        //Aguardando a conclusão da execução das threads
-        try {
-            //accessTwo.join();
-            accessOne.join();
-            //accessThree.join();
-        } catch (Exception e) {
-            e.printStackTrace();
+            //Executando o metodo run da thread
+            accessOne.start();
+            accessTwo.start();
+            accessThree.start();
+
+            //Aguardando a conclusão da execução das threads
+            try {
+                //accessTwo.join();
+                accessOne.join();
+                //accessThree.join();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            //desinscrevendo client
+            client1.unsubscribe(brokerAddress, brokerPort);
+            client2.unsubscribe(brokerAddress, brokerPort);
+            client3.unsubscribe(brokerAddress, brokerPort);
+
+            client1.stopPubSubClient();
+            client2.stopPubSubClient();
+            client3.stopPubSubClient();
         }
-
-        //desinscrevendo client
-        //client1.unsubscribe(brokerAddress, brokerPort);
-        //client2.unsubscribe(brokerAddress, brokerPort);
-        //client3.unsubscribe(brokerAddress, brokerPort);
-
-        //client1.stopPubSubClient();
-        //client2.stopPubSubClient();
-        //client3.stopPubSubClient();
-
-       // System.exit(0);
     }
 
     public static void main(String[] args) {
@@ -141,16 +140,100 @@ public class OneAppl {
 
             List<Message> log = c.getLogMessages();
             //Análisando o log, decidimos se é a hora ou não do client publicar na variavel
-            log = c.getLogMessages();
-            Iterator<Message> it2 = log.iterator();
+            while (true){
+                try {
+                    Thread.currentThread().sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                log = c.getLogMessages();
+                //Iterador para buscar no log caso alguem tenha dado unlock
+                Iterator<Message> it = log.iterator();
+                it = log.iterator();
 
-            while (it2.hasNext()) {
-                Message aux = it2.next();
-                System.out.print(aux.getContent() + " || ");
+                while (it.hasNext()) {
+                    //Pegamos uma mensagem do log
+                    Message i = it.next();
+                    String content = i.getContent();
+                    try {
+                        //Quebramos a mensagem nos " " pois a mensagem vai ter o formato: clientY OpX unlock
+                        //String[0] = clientY String[1] = OpX String[2] = unlock
+                        if(!content.contains("publishing") && !content.contains(":")){
+                            //Queremos saber somente caso alguem tenha feito lock ou unlock. Os publising são removidos do log
+                            String[] parts = content.split(" ");
+//                            if(parts.length<3){
+//                                for (String partesA : parts) {
+//                                    System.out.println ("PARTS: "+ partesA + " " +parts.length);
+//                                }
+//                            }
+
+                            if (parts.length == 3 && parts[2].equals("unlock")) {
+                                //Se alguem deu unlock, quer dizer que a operação realizada já foi finalizada, então iremos remover os dados dessa operação do log
+                                Iterator<Message> itRem = log.iterator();
+                                while (itRem.hasNext()) {
+                                    Message j = itRem.next();
+                                    String contentRem = j.getContent();
+                                    if(!content.contains("publishing")){
+                                        String[] partsRem = contentRem.split(" ");
+                                        if (partsRem.length == 3 && (partsRem[0]+partsRem[1]).equals((parts[0]+parts[1]))) {
+                                            //Removemos tanto a mensagem de unlock para a operação/cliente encontrado quando unlock
+                                            log.remove(j);
+                                            log.remove(i);
+                                            break;
+                                        }
+                                    }
+                                }
+                            } else {
+                                if (parts.length < 3) {
+                                    log.remove(i);
+                                }
+                            }
+                        }else{
+                            log.remove(i);
+                        }
+                        Iterator<Message> it2 = log.iterator();
+                        while (it2.hasNext()) {
+                            System.out.print(it2.next().getContent());
+                        }
+                        System.out.println();
+                    } catch (Exception e) {
+                        //Se encontramos um transação de unlock sem um par lock, ela tambem é removida, porem isso quer dizer que
+                        //alguma desincronização ocorreu.
+                        log.remove(i);
+                        e.printStackTrace();
+                    }
+                }
+
+                //Depois de termos removido todas as operações finalizadas do log, verificamos se é a vez do client atual de utilizar a variavel
+                String isMyTime = null;
+                try {
+                    isMyTime = log.get(0).getContent();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                //Se o lock estiver no nome do client atual
+                if (isMyTime.equals(clientName + "intent_to_lock")) {
+                    //Um publish é realizado no topico
+                    Thread publishing = new ThreadWrapper(c, "locked to "+c.getClientName()+" Op "+Integer.toString(c.getOperationId())+" - publishing on "+this.topic, host, port);
+                    publishing.start();
+                    try {
+                        publishing.join();
+                        Thread.currentThread().sleep(1000);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    //Após o publish, um unlock é realizado
+                    Thread unlock = new ThreadWrapper(c, "unlock", host, port);
+                    unlock.start();
+                    try {
+                        unlock.join();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                }
+
             }
-            System.out.println();
-
-
             //Impressão dos logs de operação para cada client
             log = c.getLogMessages();
             Iterator<Message> it = log.iterator();
